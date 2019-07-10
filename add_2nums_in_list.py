25c67715cd2108437921d3ec452c3e8022561499
gal_forer <forer.gal@gmail.com>
2019-07-10 13:57:10 +0300

    added condition to find sum

--- add_2nums_in_list.py
+++ add_2nums_in_list.py
@@ -1,19 +1,19 @@
 
-n = 23
-arr = [1, 1, 1, 2, 5, 5, 6, 8, 10, 5, 19]
+n = 10
+arr = [1, 1, 1, 2, 5, 5, 6, 7, 10, 19, 19]
 # for n in arr:
 #     print n
 
 prev = arr[0]-1
+res = False
 for num in arr:
     if num > n:
-        print 'false'
         break
     else:
         if num != prev:
-            if n-num in arr:
-                print 'true'
+            if n-num in arr and n-num != num:
+                res = True
                 break
             prev = num
-print 'false'
+print res
 
